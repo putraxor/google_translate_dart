50dd99b5a2ca5d95c1ad14498498e77c6626b73a
Putra Ardiansyah <putraxor@gmail.com>
2017-12-24 01:40:58 +0700

    "Update example"

--- example/example.dart
+++ example/example.dart
@@ -2,7 +2,13 @@ import 'package:google_translate_dart/libraries.dart';
 
 main() async {
   var gt = new GoogleTranslate();
-  var q = "Apa manfaat toge";
-  var result = await gt.translate(source: 'id', target: 'en', text: q);
-  print(result);
+
+  translate(String q) async {
+    var result = await gt.translate(source: 'id', target: 'en', text: q);
+    print(result);
+  }
+
+  await translate("Toge itu apa ya");
+  await translate("Terus manfaatnya apa");
+  await translate("Gimana cara makannya?");
 }
